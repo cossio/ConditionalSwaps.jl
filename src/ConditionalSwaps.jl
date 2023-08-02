1c43054a454cec600a00e1b54a099b4a6a9bdbe1
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-08-02 19:05:51 +0200

    swap

--- src/ConditionalSwaps.jl
+++ src/ConditionalSwaps.jl
@@ -1,5 +1,15 @@
 module ConditionalSwaps
 
-# Write your package code here.
+"""
+    swap(conditions, x, y)
 
+Returns `a, b`, where `a[i], b[i] == y[i], x[i]` or `a[i], b[i] == x[i], y[i]`,
+depending to whether the corresponding `condition[i]` is true or false.
+"""
+function swap(conditions, x, y)
+    x_new = ifelse.(conditions, y, x)
+    y_new = ifelse.(conditions, x, y)
+    return x_new, y_new
 end
+
+end # module
